7716e4c8484c8d7892b490e853377b8a847fe44c
Clare <nervous_testpilot@mail.ru>
2025-05-15 19:11:41 +0500

    сделала возможность задавать конкретный пиксель

---
+++ main.js
@@ -0,0 +1,21 @@
+import {World} from "./world.js";
+import {Hero} from "./hero.js";
+export const TILE_SIZE = 32;
+export const COLS = 15;
+export const ROWS = 20;
+
+const GAME_WIDTH = TILE_SIZE * COLS;
+const GAME_HEIGHT = TILE_SIZE * ROWS;
+
+window.addEventListener('load', function () {
+    const canvas = document.querySelector('#canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = GAME_WIDTH;
+    canvas.height = GAME_HEIGHT;
+
+    const world = new World();
+    world.drawGrid(ctx);
+
+    const myHero = new Hero();
+    myHero.draw(ctx);
+})
